609c888d423562cc1b0ed170e0ba765c91b1fecc
Miguelcrack09 <m.andres09@hotmail.com>
2021-12-05 23:36:02 -0500

    Heroku backend config

--- Backend/index.js
+++ Backend/index.js
@@ -2,7 +2,7 @@ const APIRouters = require('./routes');
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 8080;
 //const axios = require('axios').default;
 
 app.use(express.json());
